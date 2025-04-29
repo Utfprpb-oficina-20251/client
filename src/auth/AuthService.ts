8628940b077041e1fe9c5227bd9474c2096814b1
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-04-29 00:21:40 -0300

    Fechamento da sprint (#2)
    * PEXT-7 Create auth protection and related components (#1)
    
    * PEXT-29 exclusao de env file
    
    * Create main.yml
    
    * Create sonar-project.properties
    
    * PEXT-7 Create auth protection and related components
    
    * feat: Implement AuthService with signup, login, and authentication methods
    
    - Added AuthService for user authentication including signup and login functionalities.
    - Implemented methods to check if a user is authenticated and validate the authentication token.
    - Created unit tests for AuthService covering all methods and scenarios.
    - Added example test file for future test cases.
    
    ---------
    
    Co-authored-by: Rodrigo Izidoro <rodizipa2007@gmail.com>
    
    * chore - Script para facilitar deploy de arquivo estático
    
    * chore - Script para facilitar deploy de arquivo estático
    
    * chore - removed pnpm lock
    
    * chore - adicionado serve no package.json
    
    * chore - sonar.yml rename
    
    ---------
    
    Co-authored-by: Geovane Soares <57137423+geovanedcs@users.noreply.github.com>

---
+++ src/auth/AuthService.ts
@@ -0,0 +1,68 @@
+import { api } from '../libs/axios.ts'
+import { IUserSignIn } from '../commons/interfaces.ts'
+
+const signup = async (user: IUserSignIn): Promise<any> => {
+  let response
+  try {
+    response = await api.post('/users', user)
+  } catch (error: any) {
+    response = error.response
+  }
+  return response
+}
+const login = async (user: IUserSignIn): Promise<any> => {
+  let response
+  try {
+    response = await api.post('/login', user)
+    if (response.status === 200) {
+      localStorage.setItem('token', JSON.stringify(response.data.token))
+      api.defaults.headers.common['Authorization'] =
+        `Bearer ${response.data.token}`
+      localStorage.setItem('user', JSON.stringify(user.username))
+    }
+  } catch (error: any) {
+    response = error.response
+  }
+  return response
+}
+
+const isAuthenticated = (): boolean => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(token)}`
+    return true
+  }
+  return false
+}
+
+const isAuthenticatedTokenValid = async (): Promise<boolean> => {
+  const token = localStorage.getItem('token')
+  try {
+    if (token) {
+      api.defaults.headers.common['Authorization'] =
+        `Bearer ${JSON.parse(token)}`
+      const response = await api.get('/users/validatetoken')
+      if (response.status === 200) {
+        return true
+      }
+    }
+    return false
+  } catch (error: any) {
+    return false
+  }
+}
+
+const logout = (): void => {
+  localStorage.removeItem('token')
+  api.defaults.headers.common['Authorization'] = ''
+}
+
+const AuthService = {
+  signup,
+  login,
+  logout,
+  isAuthenticated,
+  isAuthenticatedTokenValid,
+}
+
+export default AuthService
---
+++ src/commons/interfaces.ts
@@ -0,0 +1,10 @@
+export interface IUserSignIn {
+  displayName: string
+  username: string
+  password: string
+}
+
+export interface IUserSignUp {
+  username: string
+  password: string
+}
