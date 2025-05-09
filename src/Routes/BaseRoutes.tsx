404fe9efda8dd708c015285297afc0d1554c3b29
Geovane Soares <57137423+geovanedcs@users.noreply.github.com>
2025-05-09 17:54:23 -0300

    PEXT-39 Add CadastroServidorPage and integrate with routing

--- src/Routes/BaseRoutes.tsx
+++ src/Routes/BaseRoutes.tsx
@@ -2,6 +2,7 @@ import { AuthenticatedRoutes } from './AuthenticatedRoutes.tsx'
 import { Route, Routes } from 'react-router-dom'
 import { HomeProjetosListPage } from '../projetos/HomeProjetosListPage.tsx'
 import { TestPage } from '../pages/TestPage.tsx'
+import { CadastroServidorPage } from '@/servidor/CadastroServidorPage.tsx'
 
 export function BaseRoutes() {
   return (
@@ -10,6 +11,7 @@ export function BaseRoutes() {
         {/*Public Routes*/}
         <Route path="/" element={<HomeProjetosListPage />} />
         <Route path="/TestPage" element={<TestPage />} />
+        <Route path="/cadastro" element={<CadastroServidorPage />} />
         {/*Private Routes*/}
         <Route element={<AuthenticatedRoutes />} />
       </Routes>
--- src/commons/interfaces.ts
+++ src/commons/interfaces.ts
@@ -54,3 +54,23 @@ export interface ErrorResponse {
   url: string
   validationErrors: string[]
 }
+
+export enum Departamentos {
+  CALEM = 'Centro Acadêmico Lingua Estrangeira Moderna',
+  DAADM = 'Departamento de Administração',
+  DAAGR = 'Departamento de Agronomia',
+  DACOC = 'Departamento de ',
+  DACON = 'Departamento de Ciências Contábeis',
+  DAELE = 'Departamento de Engenharia Elétrica',
+  DAFIS = 'Departamento de Física',
+  DAGRO = 'Departamento de Agronomia',
+  DAHUM = 'Departamento de Ciências Humanas',
+  DAINF = 'Departamento de Ciência da Computação',
+  DALET = 'Departamento de Letras',
+  DAMAT = 'Departamento de Matemática',
+  DAMEC = 'Departamento de Engenharia Mecânica',
+  DAQUI = 'Departamento de Química',
+  LATO_SENSU = 'Departamento de Pós-Graduação Lato Sensu',
+  NUAPE = 'Nucleo de Apoio Pedagógico',
+  STRICTO = 'Departamento de Pós-Graduação Stricto Sensu',
+}
